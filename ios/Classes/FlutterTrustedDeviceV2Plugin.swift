3fa553e04cbf5232271b30225f62a89aa79ac57a
Widi <42864670+SRW2009@users.noreply.github.com>
2023-09-08 10:14:49 +0700

    native ios updated

--- ios/Classes/FlutterTrustedDeviceV2Plugin.swift
+++ ios/Classes/FlutterTrustedDeviceV2Plugin.swift
@@ -16,12 +16,78 @@ public class FlutterTrustedDeviceV2Plugin: NSObject, FlutterPlugin {
         Fazpass.shared.`init`(publicAssetName: assetName)
         result(nil)
     case "generateMeta":
-        Fazpass.shared.generateMeta { meta, error in
-            guard let e = error else {
+        Fazpass.shared.generateMeta { meta, fazpassError in
+            guard let error = fazpassError else {
                 result(meta)
                 return
             }
-            result(FlutterError(code: "0", message: e.localizedDescription, details: nil))
+            
+            switch (error) {
+            case .biometricNoneEnrolled:
+                result(
+                    FlutterError(
+                        code: "fazpassE-biometricNoneEnrolled",
+                        message: "Device can't start biometry authentication because there is no biometry (Touch ID or Face ID) or device passcode enrolled.",
+                        details: nil
+                    )
+                )
+            case .biometricAuthFailed:
+                result(
+                    FlutterError(
+                        code: "fazpassE-biometricAuthFailed",
+                        message: "Biometry authentication is finished with an error (e.g. User cancelled biometric auth, etc).",
+                        details: nil
+                    )
+                )
+            case .biometricNotAvailable:
+                result(
+                    FlutterError(
+                        code: "fazpassE-biometricUnavailable",
+                        message: "Device can't start biometry authentication because biometry is unavailable.",
+                        details: nil
+                    )
+                )
+            case .biometricNotInteractive:
+                result(
+                    FlutterError(
+                        code: "fazpassE-biometricUnsupported",
+                        message: "Device can't start biometry authentication because displaying the required authentication user interface is forbidden. To fix this, you have to permit the display of the authentication UI by setting the interactionNotAllowed property to false.",
+                        details: nil
+                    )
+                )
+            case .encryptionError(let message):
+                result(
+                    FlutterError(
+                        code: "fazpassE-encryptionError",
+                        message: "Encryption went wrong because you used the wrong public key. message: \(message)",
+                        details: nil
+                    )
+                )
+            case .publicKeyNotExist:
+                result(
+                    FlutterError(
+                        code: "fazpassE-publicKeyNotExist",
+                        message: "Public key with the name registered in init method doesn't exist as an asset.",
+                        details: nil
+                    )
+                )
+            case .uninitialized:
+                result(
+                    FlutterError(
+                        code: "fazpassE-uninitialized",
+                        message: "Fazpass init method hasn't been called once.",
+                        details: nil
+                    )
+                )
+            case .unknownError(let error):
+                result(
+                    FlutterError(
+                        code: "fazpassE-unknown",
+                        message: "\(error)",
+                        details: nil
+                    )
+                )
+            }
         }
     case "enableSelected":
         let args = call.arguments as! Array<String>
